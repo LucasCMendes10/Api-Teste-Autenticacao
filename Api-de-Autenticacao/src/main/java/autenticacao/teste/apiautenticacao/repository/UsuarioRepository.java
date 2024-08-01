1b7ab51b923044a0c45ce98fd0e43905a5792102
LucasCMendes10 <lucas.mendes@sptech.school>
2024-08-01 12:26:00 -0300

    REFACTOR - fazendo o projeto voltar para o status inicial
    Ou seja, apagando tudo que fiz, incluindo testes, deixando somente o main

--- Api-de-Autenticacao/src/main/java/autenticacao/teste/apiautenticacao/repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package autenticacao.teste.apiautenticacao.repository;
-
-import autenticacao.teste.apiautenticacao.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-
-    Usuario findByEmail(String email);
-}
