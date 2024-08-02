22a01ca6beae51eba116e5a554a8e8ec9d7e68cb
LucasCMendes10 <lucas.mendes@sptech.school>
2024-08-02 14:59:16 -0300

    REFACTOR - mudando o tipo de dado do id de user e tweet para long

--- Api-de-Autenticacao/src/main/java/autenticacao/teste/apiautenticacao/repository/TweetRepository.java
+++ Api-de-Autenticacao/src/main/java/autenticacao/teste/apiautenticacao/repository/TweetRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
-public interface TweetRepository extends JpaRepository<Tweet, UUID> {
+public interface TweetRepository extends JpaRepository<Tweet, Long> {
 }
