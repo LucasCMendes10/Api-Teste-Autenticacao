bf1e2da5a6ccdd9bf8f38ae3ddd9ff310ed86c6f
LucasCMendes10 <lucas.mendes@sptech.school>
2024-08-05 10:05:42 -0300

    FIX - tirando UUID que ficou inutilizado

--- Api-de-Autenticacao/src/main/java/autenticacao/teste/apiautenticacao/dto/TweetResponseDto.java
+++ Api-de-Autenticacao/src/main/java/autenticacao/teste/apiautenticacao/dto/TweetResponseDto.java
@@ -4,12 +4,11 @@ import autenticacao.teste.apiautenticacao.model.User;
 import lombok.Data;
 
 import java.time.LocalDateTime;
-import java.util.UUID;
 
 @Data
 public class TweetResponseDto {
 
-    private UUID id;
+    private Long id;
     private User user;
     private String content;
     private LocalDateTime dtCriacao;
